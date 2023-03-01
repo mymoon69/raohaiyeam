63a23ac75dc4c1dfdc8861a2bb647f2aeadb2789
whiskas69 <88488048+whiskas69@users.noreply.github.com>
2023-03-02 01:24:02 +0700

    Add files via upload

--- phones.js
+++ phones.js
@@ -1,4 +1,4 @@
-const Samsung = JSON.parse(`[
+const phone = JSON.parse(`[
     {
         "id" : 1 ,
         "model" : "Samsung Galaxy S20 Ultra",
@@ -19,102 +19,45 @@ const Samsung = JSON.parse(`[
         "model" : "Samsung Galaxy S23 Ultra",
         "rent" : 2500 ,
         "deposit" : 1000
-    }
-]`);
-const Huawei = JSON.parse(`[
-    {
-        "id" : 1,
+    },{
+        "id" : 5,
         "model" : "HUAWEI P40 Pro+",
         "rent" : 1500,
         "deposit" : 500
     },{
-        "id" : 2,
+        "id" : 6,
         "model" : "HUAWEI P50 Pro",
         "rent" : 1500,
         "deposit" : 500
     },{
-        "id" : 3,
+        "id" : 7,
         "model" : "HUAWEI Mate40 Pro",
         "rent" : 1500,
         "deposit" : 500
     },{
-        "id" : 4,
+        "id" : 8,
         "model" : "HUAWEI Mate50 Pro",
         "rent" : 1500,
         "deposit" : 500
-    }
-]`);
-const Iphone = JSON.parse(`[
-    {
-        "id" : 1 ,
+    },    {
+        "id" : 9,
         "model" : "IPhone 13 Pro",
         "rent" : 1500 ,
         "deposit" : 500
     },{
-        "id" : 2 ,
+        "id" : 10,
         "model" : "IPhone 13 Pro Max",
         "rent" : 1500 ,
         "deposit" : 500
     },{
-        "id" : 3 ,
+        "id" : 11,
         "model" : "IPhone 14 Pro",
         "rent" : 1500 ,
         "deposit" : 500
     },{
-        "id" : 4 ,
+        "id" : 12,
         "model" : "IPhone 14 Pro Max",
         "rent" : 1500 ,
         "deposit" : 500
-    }
-]`);
-const Other = JSON.parse(`[
-    {
-        "id" : 1,
-        "brand" : "Vivo",
-        "model" : "Vivo X80 Pro",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 2,
-        "brand" : "Vivo",
-        "model" : "Vivo X70 Pro",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 3,
-        "brand" : "Vivo",
-        "model" : "Vivo X70",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 4,
-        "brand" : "Realme",
-        "model" : "realme X3 SuperZoom",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 5,
-        "brand" : "Oppo",
-        "model" : "OPPO Find X3 Pro 5G",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 6,
-        "brand" : "Oppo",
-        "model" : "OPPO Find x2 Pro",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 7,
-        "brand" : "Oppo",
-        "model" : "OPPO Reno7 5G",
-        "rent" : 1000 ,
-        "deposit" : 250
-    },{
-        "id" : 8,
-        "brand" : "Sony",
-        "model" : "Sony Xperia 1 III",
-        "rent" : 1000 ,
-        "deposit" : 250
     }
 ]`);
