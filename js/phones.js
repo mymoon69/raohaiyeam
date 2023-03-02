6d12603cfc5cf8f99d8013461e598e94ab5a6924
mymoon69 <88487704+mymoon69@users.noreply.github.com>
2023-03-03 00:16:52 +0700

    Add files via upload

---
+++ js/phones.js
@@ -0,0 +1,75 @@
+const phones = JSON.parse(`[
+    {
+        "id" : 1 ,
+        "model" : "Samsung Galaxy S20 Ultra",
+        "img" : "img/Samsung Galaxy S20 Ultra.png",
+        "rent" : 2500 ,
+        "deposit" : 1000
+    },{
+        "id" : 2 ,
+        "model" : "Samsung Galaxy S21 Ultra",
+        "img" : "img/Samsung Galaxy S21 Ultra.png",
+        "rent" : 2500 ,
+        "deposit" : 1000
+    },{
+        "id" : 3 ,
+        "model" : "Samsung Galaxy S22 Ultra",
+        "img" : "img/Samsung Galaxy S22 Ultra.png",
+        "rent" : 2500 ,
+        "deposit" : 1000
+    },{
+        "id" : 4,
+        "model" : "Samsung Galaxy S23 Ultra",
+        "img" : "img/Samsung Galaxy S23 Ultra.png",
+        "rent" : 2500 ,
+        "deposit" : 1000
+    },{
+        "id" : 5,
+        "model" : "HUAWEI P40 Pro+",
+        "img" : "img/HUAWEI P40 Pro+.png",
+        "rent" : 1500,
+        "deposit" : 500
+    },{
+        "id" : 6,
+        "model" : "HUAWEI P50 Pro",
+        "img" : "img/HUAWEI P50 Pro.png",
+        "rent" : 1500,
+        "deposit" : 500
+    },{
+        "id" : 7,
+        "model" : "HUAWEI Mate40 Pro",
+        "img" : "img/HUAWEI Mate40 Pro.png",
+        "rent" : 1500,
+        "deposit" : 500
+    },{
+        "id" : 8,
+        "model" : "HUAWEI Mate50 Pro",
+        "img" : "img/HUAWEI Mate50 Pro.png",
+        "rent" : 1500,
+        "deposit" : 500
+    },    {
+        "id" : 9,
+        "model" : "IPhone 13 Pro",
+        "img" : "img/IPhone 13 Pro.png",
+        "rent" : 1500 ,
+        "deposit" : 500
+    },{
+        "id" : 10,
+        "model" : "IPhone 13 Pro Max",
+        "img" : "img/IPhone 13 Pro Max.png",
+        "rent" : 1500 ,
+        "deposit" : 500
+    },{
+        "id" : 11,
+        "model" : "IPhone 14 Pro",
+        "img" : "img/IPhone 14 Pro.png",
+        "rent" : 1500 ,
+        "deposit" : 500
+    },{
+        "id" : 12,
+        "model" : "IPhone 14 Pro Max",
+        "img" : "img/IPhone 14 Pro Max.png",
+        "rent" : 1500 ,
+        "deposit" : 500
+    }
+]`);
