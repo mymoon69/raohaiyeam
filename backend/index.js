4f7e7d8b6da897589cd75ea151082a42eab60728
whiskas69 <64070075@it.kmitl.ac.th>
2023-05-14 00:06:33 +0700

    color button

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// Statics
+app.use(express.static('static'))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// routers
+const indexRouter = require('./routes/index')
+// const blogRouter = require('./routes/blog')
+// const commentRouter = require('./routes/comment')
+// const imageRouter = require('./routes/image')
+// const userRouter = require('./routes/user')
+
+app.use(indexRouter.router)
+// app.use(blogRouter.router)
+// app.use(commentRouter.router)
+// app.use(imageRouter.router)
+// app.use(userRouter.router)
 
 app.listen(3001, () => {
-  console.log('Start server at port 3001.')
+  console.log('Example app listening at http://localhost:3001')
 })
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -5,24 +5,24 @@
     </div>
     <div class="container">
       <!-- search -->
-      <div class="columns">
-        <div class="column is-half">
+      <div class="columns is-multiline">
+        <div class="column is-11">
           <input
-            class="input"
+            class="input is-rounded is-hovered"
             type="text"
             v-model="search"
-            placeholder="Search blog(s)"
+            placeholder="Search"
           />
         </div>
-        <div class="column is-half">
-          <button @click="getBlogs" class="button">Search</button>
+        <div class="column is-1">
+          <button @click="getBlogs" class="button is-warning">Search</button>
         </div>
       </div>
       <div class="columns is-multiline">
         <!-- brand -->
         <!-- Card element start here------------------------------------------>
         <div id="card_product" class="column is-one-quarter">
-          <div class="card" style="width:300px">
+          <div class="card" style="width: 300px">
             <div class="card-image" style="padding-top: 5%">
               <figure class="image is-1by1">
                 <img
@@ -42,11 +42,10 @@
                   <button class="button is-warning">iphone 11</button>
                 </div> -->
               </div>
-              <button class="button is-warning is-fullwidth">จองเลย</button>
+              <button class="button is-success is-fullwidth is-outlined">จองเลย</button>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   </div>
@@ -62,7 +61,7 @@ export default {
 
 <style scoped>
 .container {
-  background-color: blueviolet;
+  /* background-color: blueviolet; */
   width: 100%;
   padding-top: 5%;
 }
